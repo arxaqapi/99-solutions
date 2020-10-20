e0d0397987fd48ec7151424cc1c32d5070826448
arxaqapi <arxaqapi@protonmail.com>
2020-10-20 19:10:02 +0200

    fixed typo

--- ocaml/1_04.ml
+++ ocaml/1_04.ml
@@ -1,8 +1,8 @@
-let rec lenght l = match l with
+let rec length l = match l with
   | [] -> 0
-  | _ :: tl -> 1 + lenght tl
+  | _ :: tl -> 1 + length tl
 
-let lenght_tail_recursive l = 
+let length_tail_recursive l = 
   let rec go lst a = match lst with
     | [] -> a
     | _ :: tl -> go tl (a + 1)
@@ -12,4 +12,4 @@ let lenght_tail_recursive l =
 
 let () =
   let a = 12::3456::54::3::212::345::67::8::765::43::[] in
-  Printf.printf "length of [12;3456;54;3;212;345;67;8;765;43 = %d | tail recursion version = %d\n" (lenght a) (lenght_tail_recursive a)
+  Printf.printf "length of [12;3456;54;3;212;345;67;8;765;43 = %d | tail recursion version = %d\n" (length a) (length_tail_recursive a)
