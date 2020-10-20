55a8d1d49c6e4a01666b00ecd9f300d330703f63
arxaqapi <arxaqapi@protonmail.com>
2020-10-20 13:27:25 +0200

    removed overhead and fixed minor errors

--- ocaml/1_01.ml
+++ ocaml/1_01.ml
@@ -1,12 +1,9 @@
-let last list = 
-  let rec la l = match l with
-    | [] -> failwith "Failure, could not get last element of empty list"
-    | le :: [] -> le
-    | hd :: tl  -> la tl
-  in
-  la list
+let rec last l = match l with
+  | [] -> failwith "Failure, could not get last element of empty list"
+  | le :: [] -> le
+  | _ :: tl  -> last tl
 
 
 let () = 
-  let a = 1 :: 2 :: 3 ::[] and b = 'z' :: [] (* and c = []*) in
-  Printf.printf "head a = %d | head b = %c\n" (head a) (head b);
+  let a = 1 :: 2 :: 3 ::[] and b = 'z' :: [] in
+  Printf.printf "last [1; 2; 3] = %d | last ['z'] = %c\n" (last a) (last b);
